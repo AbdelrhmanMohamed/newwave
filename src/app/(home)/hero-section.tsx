3f536607a59a42cdf90c9f9f19d8679daff560d7
AbdelrhmanMohamed <boodyhope@gmail.com>
2025-05-20 16:01:40 +0300

    fix

--- src/app/(home)/hero-section.tsx
+++ src/app/(home)/hero-section.tsx
@@ -14,7 +14,7 @@ export default function HeroSection() {
   const parts = (hero?.title || "").split(hero?.highlight_text || "");
 
   const imageUrl = hero?.cover?.hash
-    ? `${process.env.NEXT_PUBLIC_API_URL}${hero?.cover?.hash}`
+    ? `${process.env.NEXT_PUBLIC_API_URL}/${hero?.cover?.hash}`
     : null;
 
   return (
