d7264e488114596c3f9553dd843ccc89a7fa24d1
AbdelrhmanMohamed <boodyhope@gmail.com>
2025-06-12 06:46:08 +0300

    fix: enhance metadata generation by adding Open Graph and Twitter card support with image handling

--- src/lib/shared/metadata.ts
+++ src/lib/shared/metadata.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 // import { strapiImage } from "../strapi/strapiImage";
 
+import { getImageUrl } from "../utils";
+
 // import { getImageUrl } from "../utils";
 
 export interface SEO_Response {
@@ -90,42 +92,42 @@ export function generateMetadataObject(seo: SEO_Response) {
   return {
     title: seo?.metaTitle || "Default Title", // Fallback to 'Default Title' if title is not provided
     description: seo?.metaDescription || "Default Description", // Fallback to 'Default Description'
-    keywords: seo?.keywords?.split(",") || ["default", "keywords"],
+    keywords: seo?.keywords,
     robots: seo?.metaRobots || "index, follow", // Fallback to 'index, follow'
     canonical: seo?.canonicalURL || "https://newwave.com", // Fallback to 'https://newwave.com'
-    // openGraph: {
-    //   title: seo?.metaTitle,
-    //   description: seo?.metaDescription,
-    //   url: seo?.canonicalURL || "https://newwave.com",
-    //   siteName: "NewWave",
-    //   type: "article",
-    //   images: [
-    //     {
-    //       url: getImageUrl(seo?.metaImage?.url) || "",
-    //       width: seo?.metaImage?.width || 1200,
-    //       height: seo?.metaImage?.height || 630,
-    //       alt:
-    //         seo?.metaImage?.alternativeText ||
-    //         seo?.metaTitle ||
-    //         "Default Alt Text",
-    //     },
-    //   ],
-    // },
-    // twitter: {
-    //   card: "summary_large_image",
-    //   title: seo?.metaTitle,
-    //   description: seo?.metaDescription,
-    //   images: [
-    //     {
-    //       url: getImageUrl(seo?.metaImage.url) || "",
-    //       width: seo?.metaImage?.width || 1200,
-    //       height: seo?.metaImage?.height || 630,
-    //       alt:
-    //         seo?.metaImage?.alternativeText ||
-    //         seo?.metaTitle ||
-    //         "Default Alt Text",
-    //     },
-    //   ],
-    // },
+    openGraph: {
+      title: seo?.metaTitle,
+      description: seo?.metaDescription,
+      url: seo?.canonicalURL || "https://newwave.com",
+      siteName: "NewWave",
+      type: "article",
+      images: [
+        {
+          url: getImageUrl(seo?.metaImage?.url) || "",
+          width: seo?.metaImage?.width || 1200,
+          height: seo?.metaImage?.height || 630,
+          alt:
+            seo?.metaImage?.alternativeText ||
+            seo?.metaTitle ||
+            "Default Alt Text",
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: seo?.metaTitle,
+      description: seo?.metaDescription,
+      images: [
+        {
+          url: getImageUrl(seo?.metaImage.url) || "",
+          width: seo?.metaImage?.width || 1200,
+          height: seo?.metaImage?.height || 630,
+          alt:
+            seo?.metaImage?.alternativeText ||
+            seo?.metaTitle ||
+            "Default Alt Text",
+        },
+      ],
+    },
   };
 }
